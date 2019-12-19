f89020fcbcbbe2140d7cd7a245909e60c72dacb3
jin9173 <jinhappier2@gmail.com>
2019-12-19 13:50:28 +0900

    ManyToOne recursive

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -7,6 +7,10 @@ class Person(models.Model):
         ('M', 'Medium'),
         ('L', 'Large')
     )
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZE)
+    # choices의 DB값은 tuple의 0번째 값
+    # tuple의 1번째 값을 가져오고 싶으면, get_FOO_display()를 사용
+    #   FOO: Model의 choices를 가진 field명
+    shirt_size = models.CharField(
+        max_length=1, choices=SHIRT_SIZE, help_text='셔츠 사이즈이다')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)

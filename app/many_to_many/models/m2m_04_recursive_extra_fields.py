dbd7e0a6398158b159e5eafe01fb1ecd667560a5
jin9173 <jinhappier2@gmail.com>
2019-12-19 16:46:22 +0900

    recursive_extra_field 생성 (Facebook, Instagram)

---
+++ app/many_to_many/models/m2m_04_recursive_extra_fields.py
@@ -0,0 +1,40 @@
+"""
+인스타그램 사용자
+    언제 친구를 맺었는지 (created_date)를 저장하도록 한다
+    Fields
+        from_user
+        to_user
+        created_date
+"""
+from django.db import models
+
+
+class FacebookUser(models.Model):
+    name = models.CharField(max_length=20)
+    friends = models.ManyToManyField('self')
+
+    def __str__(self):
+        return self.name
+
+
+class InstagramUser(models.Model):
+    name = models.CharField(max_length=20)
+    following = models.ManyToManyField(
+        'self',
+        through='Relation',
+        relation_name='followers',
+        symmetrical=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Relation(models.Model):
+    me = models.ForeignKey(InstagramUser, related_name='me_relation_set', on_delete=models.CASCADE)
+    counterpart = models.ForeignKey(InstagramUser, related_name='counterpart_relation_set', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Relation (me: {self.me.name}, counterpart: {self.counterpart.name})'
+

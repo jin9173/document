1bea6f0fed1a4fdea9d0a0e633ec47aed14bfd47
jin9173 <jinhappier2@gmail.com>
2019-12-23 18:35:17 +0900

    Final

--- app/inheritance/proxy/models.py
+++ app/inheritance/proxy/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+#
+# class UserMixin(models.Model):
+#     # nickname = models.CharField(max_length=30, blank=True)
+#
+#     class Meta:
+#         abstract = True
+
 
 class BaseUser(models.Model):
+    is_superuser = models.BooleanField(default=False)
+
     def show_pk(self):
         print(self.pk)
     pass
@@ -12,9 +21,31 @@ class NormalUser(BaseUser):
         proxy = True
 
 
+# SuperUser모델에서 사용할 Manager
+class SuperUserManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_superuser=True)
+
+
 class SuperUser(BaseUser):
+    # 프록시 모델에 Manager를 지정
+    objects = SuperUserManager()
+
     class Meta:
         proxy = True
 
     def delete_user(self, pk):
         BaseUser.objects.get(pk=pk).delete()
+
+#
+# # Multiple inheritance
+# class Book(models.Model):
+#     pass
+#
+# #
+# # class Article(models.Model):
+# #     pass
+#
+#
+# class BookReview(Book):
+#     pass

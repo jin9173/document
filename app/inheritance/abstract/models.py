b64dac998071365db05c14be4c7837c97fa94eaa
jin9173 <jinhappier2@gmail.com>
2019-12-23 16:27:42 +0900

    Muti-table

--- app/inheritance/abstract/models.py
+++ app/inheritance/abstract/models.py
@@ -16,10 +16,18 @@ class CommonInfo(models.Model):
 
     class Meta:
         abstract = True
+        ordering = ['name']
 
 
 class Student(CommonInfo):
     school = models.CharField(max_length=30)
+
+    # 상속받은 자식 클래스에서, 부모 클래스의 Meta속성을 가지면서 자신만의 속성을 추가하고 싶다면
+    # <부모클래스>.Meta를 상속받고, 나머지 필요한 속성들을 정의하도록 한다
+    #   아래 경우, 부모로부터 ordering속성을 상속받는다
+    class Meta(CommonInfo.Meta):
+        verbose_name = '학생'
+        verbose_name_plural = '학생 목록'
 
     def __str__(self):
         return f'{self.name} (학교: {self.school})'
@@ -29,4 +37,20 @@ class Instructor(CommonInfo):
     academy = models.CharField(max_length=20)
 
     def __str__(self):
-        return f'{self.name} (학원: {self.school})'
+        return f'{self.name} (학원: {self.academy})'
+
+
+# Be careful with related_name and releated_query_name
+class Base(models.Model):
+    m2m = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_set')
+
+    class Meta:
+        abstract = True
+
+
+class ChildA(Base):
+    pass
+
+
+class ChildB(Base):
+    pass
